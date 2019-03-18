f89ab03aac063a58aa74ce7a14298d14b96b7e1a
saurin.chauhan <saurin.chauhan@einfochips.com>
2019-03-18 17:35:49 +0530

    ADDED: ssh command with .pem file

--- reverse_ssh/enable-reverse-ssh.sh
+++ reverse_ssh/enable-reverse-ssh.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+#	authentication using username and password
 ssh -N -R 7001:localhost:22 <username>@<ran.dom-s.sh>
 
+#	authentication using username and private key .pem file <used with aws instance>
+ssh -N -R 7001:localhost:22 -i ~/priveteKey.pem <username>@<ran.dom-s.sh>
